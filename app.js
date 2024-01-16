cb129aad27781e840b4e90da30ce18f85eb90a05
naathaliie <nathalielarsson@live.se>
2024-01-16 17:11:07 +0100

    my first push

---
+++ app.js
@@ -0,0 +1,3 @@
+//Skapa en array med strängarna: apa, orangutang, gorilla, geta och giraff
+var myArray = ["apa", "orangutang", "gorilla", "get", "giraff"];
+console.log(myArray);
---
+++ app.ts
@@ -0,0 +1,5 @@
+//Skapa en array med strängarna: apa, orangutang, gorilla, geta och giraff
+
+const myArray: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
+
+console.log(myArray);
