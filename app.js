7cde7a43f3a78e11e936bd6b3985892e250f4396
naathaliie <nathalielarsson@live.se>
2024-01-17 10:06:55 +0100

    includesForAndForeachLoops

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ var myObj = {
     member: true
 };
 console.log(myObj);
+/* Skriv en funktion som tar emot en sträng och returnerar en sträng.
+Om funktionen tar emot strängen “X” ska den returnera strängen “hej X”.
+Funktionen ska kunna ta emot vilken sträng som helst och skicka tillbaka en sträng med hej först. */
+function myFunction(myString) {
+    var greeting = "Hej";
+    return (greeting + " " + myString);
+}
+;
+var aString = "X";
+var anotherString = "N";
+console.log(myFunction(anotherString));
+/* Loopa igenom arrayen av strängar och kör funktionen
+på varje sträng och skriv ut till konsollen det du får
+från arrayen.*/
+//Test med en for-loop
+for (var i = 0; i < myArray.length; i++) {
+    var element = myArray[i];
+    var result = myFunction(element);
+    console.log(result);
+}
+//Test med en foreach-loop
+myArray.forEach(function (animal) {
+    var newGreeting = "Roligt att se dig";
+    var result = newGreeting + " " + myFunction(animal);
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -15,3 +15,40 @@ const myObj: {name: string, age: number, member: boolean} ={
 };
 
 console.log(myObj);
+
+
+/* Skriv en funktion som tar emot en sträng och returnerar en sträng. 
+Om funktionen tar emot strängen “X” ska den returnera strängen “hej X”. 
+Funktionen ska kunna ta emot vilken sträng som helst och skicka tillbaka en sträng med hej först. */
+
+function myFunction(myString:string): string{
+    const greeting :string = "Hej";
+
+    return (greeting + " " + myString);
+};
+
+const aString :string = "X";
+const anotherString :string ="N";
+
+console.log(myFunction(anotherString));
+
+
+
+/* Loopa igenom arrayen av strängar och kör funktionen 
+på varje sträng och skriv ut till konsollen det du får 
+från arrayen.*/
+
+//Test med en for-loop
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+
+    const result = myFunction(element);
+    console.log(result); 
+}
+
+//Test med en foreach-loop
+myArray.forEach(animal => {
+    const newGreeting :string = "Roligt att se dig";
+    const result = newGreeting + " " + myFunction(animal);
+    console.log(result);
+});
